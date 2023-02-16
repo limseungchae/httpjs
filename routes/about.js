36939bd6c05d74b49c515c961563214aa6e5c7fc
limseungchae <tmdco12@naver.com>
2023-02-16 12:47:09 +0900

    express view engine - handlebars

--- routes/about.js
+++ routes/about.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 // show about page
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'../public','about.html'));
+    // res.sendFile(path.join(__dirname,'../public','about.html'));
+    res.render('about', {title: 'about'});
 });
 
 module.exports =  router;
