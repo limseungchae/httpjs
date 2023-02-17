66b67111c9281b97ab62e27fc07fc20f1840778d
limseungchae <tmdco12@naver.com>
2023-02-17 13:55:49 +0900

    성적처리 2 - 모델객체 생성 후 성적처리

--- express04.js
+++ express04.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
-const logger = require('morgan'); // 로그 출력기
+const logger = require('morgan');  // 로그 출력기
 const { engine } = require('express-handlebars');
-const bodyParser = require('body-parser');  // 폼 처리기
+const bodyParser = require('body-parser');   // 폼 처리기
 const oracledb = require('./models/Oracle');
 
 // 라우팅 외부 작성
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // view 템플릿 엔진 설정
-app.engine('hbs',engine({
+app.engine('hbs', engine({
     extname: '.hbs', defaultLayout: 'layout',
     helpers: {
         section: function(name, options) {
@@ -25,7 +25,7 @@ app.engine('hbs',engine({
     },
 }));
 app.set('views', path.join(__dirname, 'views'));
-app.set ('view engine', 'hbs');
+app.set('view engine', 'hbs');
 
 // 라우팅을 거치지 않고 직접 호출해서 응답
 app.use(express.static(path.join(__dirname, 'static')));
@@ -35,13 +35,12 @@ app.use(logger('dev'));
 
 // 미들웨어 등록 및 설정
 app.use(express.json());
-// 전송된 폼 데이터에 대한 urlencoding 위한 설정
+// 전송된 폼 데이터에 대한 urlencoding 설정
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());   // 전송된 폼 데이터는 json형식
-// app.use(bodyParser.raw());
-// app.use(bodyParser.text());  // enctype이 text/plain일때 필요 (비추)
+// app.use(bodyParser.text());   // enctype이 text/plain일때 필요 (비추)
 
-oracledb.initConn();     // 오라클 instant client 초기화
+oracledb.initConn();  // 오라클 instant client 초기화
 
 // index에 대한 route handler 지정
 app.use('/', indexRouter);
@@ -62,5 +61,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {
-    console.log('서버가 실행중... 중지하려면 ctrl+c를 눌러요!');
+    console.log('express 서버 실행중... 중지하려면 ctrl+c를 눌러요!');
 });
